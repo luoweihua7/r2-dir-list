03450eb6a85fd8bcc2a61524742bd99ba99c0f99
Larify <luoweihua7@gmail.com>
2024-06-20 10:04:52 +0800

    fix: 修正url中的secret参数丢失的问题

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { parse } from 'cookie';
 import { Env } from './types';
 import { renderTemplFull } from './render';
 import { siteConfig } from './config';
@@ -41,21 +42,41 @@ async function listBucket(bucket: R2Bucket, options?: R2ListOptions): Promise<R2
 
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    const originResponse = await fetch(request);
+    // remove query string
+    const requestCopy = new Request(request.url.split('?').shift(), request);
+    const bucketResp = await fetch(requestCopy);
 
     // if status is not 404 or request path not end with '/', return origin response
-    if (![404].includes(originResponse.status) || originResponse.url.slice(-1) !== '/' || !siteConfig) {
-      return originResponse;
+    if (![404].includes(bucketResp.status) || bucketResp.url.slice(-1) !== '/' || !siteConfig) {
+      return bucketResp;
     }
 
-    const { pathname, searchParams } = new URL(request.url);
-    const secretKey = searchParams.get('secret');
-    const { secret = '' } = siteConfig;
+    const { hostname, pathname, searchParams } = new URL(request.url);
+    let secret = searchParams.get('secret');
+    const { secretKey = '' } = siteConfig;
 
-    if (secret && secretKey !== secret) {
-      return new Response('Unauthorized', {
-        status: 401,
-      });
+    // 增加简单的安全校验
+    if (secretKey) {
+      // 将URL中的secret参数写入到cookie中
+      if (secretKey === secret) {
+        return new Response('', {
+          status: 302,
+          headers: {
+            Location: `${pathname}`,
+            'Set-Cookie': `secret=${secret}; path=/; domain=${hostname}; SameSite=None; Secure`,
+          },
+        });
+      }
+
+      // URL中没有 secret 参数，从cookie中获取
+      const cookies = parse(request.headers.get('Cookie') || '');
+      secret = cookies['secret'];
+
+      if (secretKey !== secret) {
+        return new Response('Unauthorized', {
+          status: 401,
+        });
+      }
     }
 
     // remove the leading '/'
@@ -69,7 +90,7 @@ export default {
 
     // if no object found, return origin response
     if (index.objects.length === 0 && index.delimitedPrefixes.length === 0) {
-      return originResponse;
+      return bucketResp;
     }
 
     return new Response(renderTemplFull(index.objects, index.delimitedPrefixes, pathname, siteConfig), {
